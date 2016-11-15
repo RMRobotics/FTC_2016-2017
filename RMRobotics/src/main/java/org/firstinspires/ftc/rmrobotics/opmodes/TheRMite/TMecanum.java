d659c862ecc151bc2c4c16ce95ef85dbdb6762c8
Simon <michaelblob99@gmail.com>
2016-11-15 15:25:38 -0500

    Add resolution and toggle to drive utility

--- RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/TheRMite/TMecanum.java
+++ RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/TheRMite/TMecanum.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.rmrobotics.opmodes.TheRMite;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.rmrobotics.control.Axis;
+import org.firstinspires.ftc.rmrobotics.control.Button;
 import org.firstinspires.ftc.rmrobotics.control.Controller;
 import org.firstinspires.ftc.rmrobotics.control.Joystick;
 import org.firstinspires.ftc.rmrobotics.core.RTeleOp;
@@ -10,32 +11,25 @@ import org.firstinspires.ftc.rmrobotics.util.DriveUtil;
 import org.firstinspires.ftc.rmrobotics.util.config.Robot;
 import org.firstinspires.ftc.rmrobotics.util.config.TheRMite;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 @TeleOp(name="TMecanum", group="TheRMite")
 public class TMecanum extends RTeleOp {
 
     private TheRMite config;
+    private boolean resTog = false;
 
     @Override
     protected void calculate() {
         double forward = control.joystickValue(Controller.ONE, Joystick.LEFT, Axis.Y);
         double strafe = control.joystickValue(Controller.ONE, Joystick.LEFT, Axis.X);
         double rotate = control.joystickValue(Controller.ONE, Joystick.RIGHT, Axis.X);
-        double max = maxCheck(forward, strafe, rotate);
-
-        /**
-        config.FL().setPower((forward + strafe - rotate)/max);
-        config.FR().setPower((forward - strafe + rotate)/max);
-        config.BL().setPower((forward - strafe - rotate)/max);
-        config.BR().setPower((forward + strafe + rotate)/max);
-        **/
 
         double driveWeight = 1.0;
         double turnWeight = 1.0;
-        double[] voltages = DriveUtil.mecanumDrive(strafe, forward, driveWeight, rotate, turnWeight);
+        double res = 0.4;
+        if (control.buttonPressed(Controller.ONE, Button.BACK)) {
+            resTog = !resTog;
+        }
+        double[] voltages = DriveUtil.mecanumDrive(strafe, forward, driveWeight, rotate, turnWeight, res, resTog);
         config.FL().setPower(voltages[0]);
         config.FR().setPower(voltages[1]);
         config.BL().setPower(voltages[2]);
@@ -47,19 +41,6 @@ public class TMecanum extends RTeleOp {
     @Override
     protected Robot setRobot() {
         return config = new TheRMite(hardwareMap);
-    }
-
-    private double maxCheck(double f, double s, double r) {
-        List l = new ArrayList();
-        l.add(Math.abs(f + s + r));
-        l.add(Math.abs(f - s - r));
-        l.add(Math.abs(f - s + r));
-        l.add(Math.abs(f + s - r));
-        if ((double) Collections.max(l) > 1) {
-            return (double) Collections.max(l);
-        } else {
-            return 1;
-        }
     }
 
     @Override
--- RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/util/DriveUtil.java
+++ RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/util/DriveUtil.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.rmrobotics.util;
 public class DriveUtil {
 
     //Return format {r, theta}
-    public static double[] cartesianToPolar(double x, double y){
+    private static double[] cartesianToPolar(double x, double y){
         double r = Math.sqrt(Math.pow(x, 2.0) + Math.pow(y, 2.0));
         double theta = Math.atan2(y, x);
         if(theta < 0)
@@ -15,7 +15,7 @@ public class DriveUtil {
     //    1   2
     //    3   4
     //driveWeight and turnWeight should be in [0,1]
-    public static double[] mecanumDrive(double xJoy, double yJoy, double driveWeight, double turnJoy, double turnWeight){
+    public static double[] mecanumDrive(double xJoy, double yJoy, double driveWeight, double turnJoy, double turnWeight, double res, boolean resTog){
         double[] polar = cartesianToPolar(xJoy, yJoy);
         double r = polar[0];
         double theta = polar[1];
@@ -28,10 +28,18 @@ public class DriveUtil {
         double v4 = weightedR*Math.sin(theta + (Math.PI/4.0)) - weightedTurn;
 
         double absMaxV =  Math.max(Math.max(Math.max(Math.abs(v1), Math.abs(v2)), Math.abs(v3)), Math.abs(v4));
-        v1 /= absMaxV;
-        v2 /= absMaxV;
-        v3 /= absMaxV;
-        v4 /= absMaxV;
+        if (absMaxV > 1.0) { //max voltage of motors is 1.0, so any values over 1.0 will be scaled down appropriately
+            v1 /= absMaxV;
+            v2 /= absMaxV;
+            v3 /= absMaxV;
+            v4 /= absMaxV;
+        }
+        if (resTog) {
+            v1 *= res;
+            v2 *= res;
+            v3 *= res;
+            v4 *= res;
+        }
         return new double[] {v1, v2, v3, v4};
     }
 
