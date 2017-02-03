632c57254ecc734ebbe1a63a568f55e5e56785db
Gregory <gregory.marinakis@gmail.com>
2017-02-03 16:11:08 -0500

    Added Backup Autonomous Object and opmode

---
+++ RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/SandstoRM/AutoFxns.java
@@ -0,0 +1,161 @@
+package org.firstinspires.ftc.rmrobotics.opmodes.SandstoRM;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by Gregory on 1/10/2017.
+ */
+
+public class AutoFxns {
+
+    private DcMotor wheelFL;
+    private DcMotor wheelFR;
+    private DcMotor wheelBL;
+    private DcMotor wheelBR;
+    private DcMotor shootL;
+    private DcMotor shootR;
+
+    private Servo index;
+    private Servo beaconL;
+    private Servo beaconR;
+
+    byte[] colorLcache;
+    byte[] colorRcache;
+    I2cDevice colorL;
+    I2cDevice colorR;
+    I2cDevice lineL;
+    I2cDevice lineR;
+    I2cDeviceSynch colorLreader;
+    I2cDeviceSynch colorRreader;
+    I2cDeviceSynch lineLreader;
+    I2cDeviceSynch lineRreader;
+    private ElapsedTime time = new ElapsedTime();
+
+
+    public AutoFxns(DcMotor a, DcMotor b, DcMotor c, DcMotor d, DcMotor e, DcMotor f, Servo aa, Servo bb, Servo cc, I2cDevice aaa, I2cDevice bbb, I2cDevice ccc, I2cDevice ddd, I2cDeviceSynch aaaa, I2cDeviceSynch bbbb, I2cDeviceSynch cccc, I2cDeviceSynch dddd)
+    {
+        wheelFL = a;
+        wheelFR = b;
+        wheelBL = c;
+        wheelBR = d;
+        shootL = e;
+        shootR = f;
+        index = aa;
+        beaconL = bb;
+        beaconR = cc;
+        colorL = aaa;
+        colorR = bbb;
+        lineL = ccc;
+        lineR = ddd;
+        colorLreader = aaaa;
+        colorRreader = bbbb;
+        lineLreader = cccc;
+        lineRreader = dddd;
+    }
+
+    public void move(double duration, double power, double angle, double rotate) {
+        angle *= (Math.PI / 180);
+        time.reset();
+        while (time.seconds() < duration) {
+            wheelFL.setPower(power * Math.sin(angle + (Math.PI / 4)) + rotate);
+            wheelFR.setPower(power * Math.cos(angle + (Math.PI / 4)) - rotate);
+            wheelBL.setPower(power * Math.cos(angle + (Math.PI / 4)) + rotate);
+            wheelBR.setPower(power * Math.sin(angle + (Math.PI / 4)) - rotate);
+        }
+    }
+
+    public void shoot(double rev, double fireTime) {
+        shootL.setPower(100);
+        shootR.setPower(100);
+        time.reset();
+        while (time.seconds() < rev) ;
+        index.setPosition(0);
+        time.reset();
+        while (time.seconds() < fireTime) ;
+        shootL.setPower(0);
+        shootR.setPower(0);
+    }
+
+    public String scanColor(String color) {
+        byte[] colorLcache = colorLreader.read(0x04, 1);
+        byte[] colorRcache = colorRreader.read(0x04, 1);
+        if (color.charAt(0) == 'r' || color.charAt(0) == 'R') {
+            //if left detects red
+            if (colorLcache[0] > colorRcache[0])
+                return "Left";
+            //if right detects red
+            if (colorLcache[0] < colorRcache[0])
+                return "Right";
+        }
+        if (color.charAt(0) == 'b' || color.charAt(0) == 'B') {
+            //if left detects blue
+            if (colorLcache[0] < colorRcache[0])
+                return "Left";
+            //if right detects blue
+            if (colorLcache[0] > colorRcache[0])
+                return "Right";
+        }
+        return "Undetermined";
+    }
+
+    public void beaconEngage(boolean set, String side) {
+        if (side.charAt(0) == 'L' && set == true)
+            beaconL.setPosition(1);
+        else if (side.charAt(0) == 'R' && set == true)
+            beaconR.setPosition(1);
+        else if (side.charAt(0) == 'L' && set == false)
+            beaconL.setPosition(0);
+        else if (side.charAt(0) == 'R' && set == false)
+            beaconL.setPosition(0);
+
+    }
+
+    public void lineCheck(double num, String side) {
+        //on red side the right side should see line first, on blue side left side should see line first
+        if (side.charAt(0) == 'r' || side.charAt(0) == 'R')
+        {
+            if (colorRreader.read(0x04, 1)[0] > 15) {
+                while (colorLreader.read(0x04, 1)[0] <= 15) {
+                    wheelFL.setPower(num);
+                    wheelFR.setPower(num);
+                    wheelBL.setPower(num);
+                    wheelBR.setPower(num);
+                }
+                time.reset();
+                while (colorRreader.read(0x04, 1)[0] <= 15) {
+                    wheelFL.setPower(-num);
+                    wheelFR.setPower(num);
+                    wheelBL.setPower(-num);
+                    wheelBR.setPower(num);
+                }
+                double turntime = time.time();
+                time.reset();
+                move((int)(turntime/2), 0, 0, num);
+            }
+        }
+        if (side.charAt(0) == 'b' || side.charAt(0) == 'B') {
+            if (colorLreader.read(0x04, 1)[0] > 15) {
+                while (colorRreader.read(0x04, 1)[0] <= 15) {
+                    wheelFL.setPower(num);
+                    wheelFR.setPower(num);
+                    wheelBL.setPower(num);
+                    wheelBR.setPower(num);
+                }
+                time.reset();
+                while (colorLreader.read(0x04, 1)[0] <= 15) {
+                    wheelFL.setPower(-num);
+                    wheelFR.setPower(num);
+                    wheelBL.setPower(-num);
+                    wheelBR.setPower(num);
+                }
+                double turntime = time.time();
+                time.reset();
+                move((int) (turntime / 2), 0, 0, num);
+            }
+        }
+    }
+}
---
+++ RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/SandstoRM/BackupAuto.java
@@ -0,0 +1,89 @@
+package org.firstinspires.ftc.rmrobotics.opmodes.SandstoRM;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by Gregory on 12/13/2016.
+ */
+@Autonomous(name="DarudeAutoSensorsR", group="SandstoRM")
+public class BackupAuto extends LinearOpMode {
+
+    private DcMotor wheelFL;
+    private DcMotor wheelFR;
+    private DcMotor wheelBL;
+    private DcMotor wheelBR;
+    private DcMotor shootL;
+    private DcMotor shootR;
+    private Servo index;
+    private Servo beaconL;
+    I2cDevice colorL;
+    I2cDevice colorR;
+    I2cDevice lineL;
+    I2cDevice lineR;
+    I2cDeviceSynch colorLreader;
+    I2cDeviceSynch colorRreader;
+    I2cDeviceSynch lineLreader;
+    I2cDeviceSynch lineRreader;
+
+    public void runOpMode()
+    {
+        wheelFL = hardwareMap.dcMotor.get("wheelFL");
+        wheelFR = hardwareMap.dcMotor.get("wheelFR");
+        wheelBL = hardwareMap.dcMotor.get("wheelBL");
+        wheelBR = hardwareMap.dcMotor.get("wheelBR");
+        shootL = hardwareMap.dcMotor.get("shootL");
+        shootR = hardwareMap.dcMotor.get("shootR");
+        wheelFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        wheelBL.setDirection(DcMotorSimple.Direction.REVERSE);
+        wheelFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wheelFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wheelBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wheelBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wheelFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        wheelFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        wheelBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        wheelBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shootL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shootR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        beaconL = hardwareMap.servo.get("beaconL");
+        index = hardwareMap.servo.get("index");
+
+        colorL = hardwareMap.i2cDevice.get("cL");
+        colorR = hardwareMap.i2cDevice.get("cR");
+        lineL = hardwareMap.i2cDevice.get("colorLineL");
+        lineR = hardwareMap.i2cDevice.get("colorLineR");
+        colorLreader = new I2cDeviceSynchImpl(colorL, I2cAddr.create8bit(0x50), false);
+        colorRreader = new I2cDeviceSynchImpl(colorR, I2cAddr.create8bit(0x52), false);
+        lineLreader = new I2cDeviceSynchImpl(lineL, I2cAddr.create8bit(0x3c), false);
+        lineRreader = new I2cDeviceSynchImpl(lineR, I2cAddr.create8bit(0x3c), false);
+        colorLreader.engage();
+        colorRreader.engage();
+        lineLreader.engage();
+        lineRreader.engage();
+        colorLreader.write8(3, 1);
+        colorRreader.write8(3, 1);
+        lineLreader.write8(3,0);
+        lineRreader.write8(3,0);
+
+        //beaconR does not exist on SandstoRM
+        AutoFxns auto = new AutoFxns(wheelFL, wheelFR, wheelBL, wheelBR, shootL, shootR, index, beaconL, null, colorL, colorR, lineL, lineR, colorLreader, colorRreader, lineLreader, lineRreader);
+        waitForStart();
+        //duration, power, strafe, rotate
+        auto.move(3.5,100,0,0);
+        //time to rev flywheel, time for ball to exit flywheel
+        auto.shoot(3,2.6);
+        auto.move(2,100,0,0);
+        auto.move(5,0,100,0);
+    }
+}
