509cc2bfee360b1e8f8e1ae1f0e5672670e0ad0a
Simon <ishayardi@gmail.com>
2017-02-04 15:36:08 -0500

    added capAutoLinear

---
+++ RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/feRMilab/CapAutoLinear.java
@@ -0,0 +1,156 @@
+package org.firstinspires.ftc.rmrobotics.opmodes.feRMilab;
+
+import com.kauailabs.navx.ftc.AHRS;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by RM Robotics on 2/4/2017.
+ */
+public class CapAutoLinear extends LinearOpMode {
+
+    private DcMotor FL;
+    private DcMotor FR;
+    private DcMotor BL;
+    private DcMotor BR;
+
+    private AHRS navx;
+
+    private DeviceInterfaceModule dim;
+    double startPos;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FL = hardwareMap.dcMotor.get("FL");
+        FR = hardwareMap.dcMotor.get("FR");
+        BL = hardwareMap.dcMotor.get("BL");
+        BR = hardwareMap.dcMotor.get("BR");
+        FR.setDirection(DcMotor.Direction.REVERSE);
+        BR.setDirection(DcMotor.Direction.REVERSE);
+        setZeroMode(DcMotor.ZeroPowerBehavior.BRAKE);
+        setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        dim = hardwareMap.deviceInterfaceModule.get("dim");
+
+        navx = AHRS.getInstance(dim, 0, AHRS.DeviceDataType.kProcessedData, (byte) 50);
+
+        while (navx.isCalibrating()) {
+            telemetry.addData("Status", !navx.isCalibrating());
+            telemetry.update();
+        }
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        dim.setLED(1, true);
+
+        navx.zeroYaw();
+
+        waitForStart();
+
+        //turn at an angle
+        turnRobotCorner(45);
+
+        //drive forward
+        driveRobot(2500, -.3);
+
+        //wait
+        sleep(3000);
+
+        //knock-off capball
+        turnRobot(500);
+
+        while (opModeIsActive()){
+            addTelemetry();
+        }
+    }
+
+    private void driveRobot(int distance, double power){
+        startPos = FL.getCurrentPosition();
+        while (FL.getCurrentPosition() - startPos > (-1*distance) && opModeIsActive()){
+            setDrive(power);
+        }
+        setDrive(0);
+        sleep(100);
+    }
+
+    private void turnRobotCorner(int a){
+        while (Math.abs(navx.getYaw() + a) > 2 && opModeIsActive()) {
+            int scale;
+            if (navx.getYaw() + a > 0) {
+                scale = -1;
+            } else {
+                scale = 1;
+            }
+            if (Math.abs(navx.getYaw()) < (a - 10)){
+                setDrive(scale * 0.25, 0);
+            } else {
+                setDrive(scale * 0.07, 0);
+            }
+        }
+        setDrive(0);
+        sleep(100);
+    }
+
+    private void turnRobot(int a){
+        while (Math.abs(navx.getYaw() + a) > 2 && opModeIsActive()) {
+            int scale;
+            if (navx.getYaw() + a > 0) {
+                scale = -1;
+            } else {
+                scale = 1;
+            }
+            if (Math.abs(navx.getYaw() + a) > 20) {
+                setDrive(scale * 0.25, scale*-0.25);
+            } else {
+                setDrive(scale * 0.07, scale*-0.07);
+            }
+        }
+        setDrive(0);
+        sleep(100);
+    }
+
+    private void addTelemetry() {
+//        telemetry.addData("1 Time", runtime.seconds());
+        telemetry.addData("2 Yaw", navx.getYaw());
+//        telemetry.addData("3 ColorBack", colorBackReader.read(0x08, 1)[0] & 0xFF);
+//        telemetry.addData("4 ColorCenter", colorCenterReader.read(0x08, 1)[0] & 0xFF);
+//        telemetry.addData("5 ColorLeft", colorLeftReader.read(0x04, 1)[0] & 0xFF);
+//        telemetry.addData("6 ColorRight", colorRightReader.read(0x04, 1)[0] & 0xFF);
+        // telemetry.addData("7 Range", rangeReader.read(0x04, 2)[0] + " " + rangeReader.read(0x04, 2)[1]);
+        telemetry.addData("8 Motor", FL.getPower() + " " + FR.getPower() + " " + BL.getPower() + " " + BR.getPower());
+        telemetry.addData("9 Encoder", FL.getCurrentPosition() + " " + FR.getCurrentPosition() + " " + BL.getCurrentPosition() + " " + BR.getCurrentPosition());
+        telemetry.update();
+    }
+
+    private void setMode(DcMotor.RunMode r) {
+        FL.setMode(r);
+        FR.setMode(r);
+        BL.setMode(r);
+        BR.setMode(r);
+    }
+
+    private void setZeroMode(DcMotor.ZeroPowerBehavior z) {
+        FL.setZeroPowerBehavior(z);
+        FR.setZeroPowerBehavior(z);
+        BL.setZeroPowerBehavior(z);
+        BR.setZeroPowerBehavior(z);
+    }
+
+    private void setDrive(double p) {
+        FL.setPower(p);
+        FR.setPower(p);
+        BL.setPower(p);
+        BR.setPower(p);
+    }
+
+    private void setDrive(double p1, double p2) {
+        FL.setPower(p1);
+        FR.setPower(p2);
+        BL.setPower(p1);
+        BR.setPower(p2);
+    }
+}
