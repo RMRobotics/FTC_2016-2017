3ad126b6cb66918884ce4c625078753939246a98
Sue Lee <Sue Lee>
2017-02-14 17:06:38 -0500

    comments added for ShootCap

--- RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/feRMilab/autored/ShootCap.java
+++ RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/feRMilab/autored/ShootCap.java
@@ -16,31 +16,38 @@ public class ShootCap extends FeRMiLinear {
     public void runOpMode() throws InterruptedException {
         super.initialize(Color.RED, DcMotor.RunMode.RUN_USING_ENCODER);
 
+        // sleep 10 seconds
         sleep(10000);
 
-        //drive
+        // drive for .7 seconds on .5 power
         setDrive(0.5);
         sleep(700);
         setDrive(0);
 
-        //shoot
+        // shoot at .95 power
         flyL.setPower(0.95);
         flyR.setPower(0.95);
+        // wait for .2 seconds for flywheel to speed up
         sleep(200);
+        // open indexer
         index.setPosition(0.5);
         sleep(200);
+        // turn on belt at power 1
         belt.setPower(1.0);
         sleep(3000);
+        //stop flywheels and belt
         flyL.setPower(0);
         flyR.setPower(0);
         belt.setPower(0);
 
-        //drive
+        //drive for 1.35 seconds on .5 power
         setDrive(.5);
         sleep(1350);
 
+        //stop drive
         setDrive(0);
 
+        //program continues to run until stop button pressed
         while (opModeIsActive()) {
             telemetry.addData("yee", "eet");
             telemetry.update();
