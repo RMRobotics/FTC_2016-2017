dd2ed4431074fea2f307cd5d7dfb45a2cbb86b54
Simon Liu <michaelblob99@gmail.com>
2017-02-14 17:07:06 -0500

    initial changes for code cleanup

--- RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/feRMilab/autored/Cap.java
+++ RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/feRMilab/autored/Cap.java
@@ -1,56 +1,20 @@
 package org.firstinspires.ftc.rmrobotics.opmodes.feRMilab.autored;
 
-import com.kauailabs.navx.ftc.AHRS;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+
+import org.firstinspires.ftc.rmrobotics.opmodes.feRMilab.FeRMiLinear;
+import org.firstinspires.ftc.rmrobotics.util.Color;
 
 /**
  * Created by RM Robotics on 2/4/2017.
  */
 @Autonomous(name = "RED: Cap")
-public class Cap extends LinearOpMode {
-
-    private DcMotor FL;
-    private DcMotor FR;
-    private DcMotor BL;
-    private DcMotor BR;
-
-    private AHRS navx;
-
-    private DeviceInterfaceModule dim;
-    double startPos;
+public class Cap extends FeRMiLinear {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        FL = hardwareMap.dcMotor.get("FL");
-        FR = hardwareMap.dcMotor.get("FR");
-        BL = hardwareMap.dcMotor.get("BL");
-        BR = hardwareMap.dcMotor.get("BR");
-        FR.setDirection(DcMotor.Direction.REVERSE);
-        BR.setDirection(DcMotor.Direction.REVERSE);
-        setZeroMode(DcMotor.ZeroPowerBehavior.BRAKE);
-        setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        dim = hardwareMap.deviceInterfaceModule.get("dim");
-
-        navx = AHRS.getInstance(dim, 0, AHRS.DeviceDataType.kProcessedData, (byte) 50);
-
-        while (navx.isCalibrating()) {
-            telemetry.addData("Status", !navx.isCalibrating());
-            telemetry.update();
-        }
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        dim.setLED(1, true);
-
-        navx.zeroYaw();
-
-        waitForStart();
+        super.initialize(Color.NEITHER, DcMotor.RunMode.RUN_USING_ENCODER);
 
         //turn at an angle
         turnRobotCorner(45);
@@ -73,7 +37,7 @@ public class Cap extends LinearOpMode {
     }
 
     private void driveRobot(int distance, double power){
-        startPos = FL.getCurrentPosition();
+        double startPos = FL.getCurrentPosition();
         while (FL.getCurrentPosition() - startPos > (-1*distance) && opModeIsActive()){
             //while robot has not traveled distance amount
             setDrive(power);
@@ -124,46 +88,5 @@ public class Cap extends LinearOpMode {
         }
         setDrive(0);
         sleep(100);
-    }
-
-    private void addTelemetry() {
-//        telemetry.addData("1 Time", runtime.seconds());
-        telemetry.addData("2 Yaw", navx.getYaw());
-//        telemetry.addData("3 ColorBack", colorBackReader.read(0x08, 1)[0] & 0xFF);
-//        telemetry.addData("4 ColorCenter", colorCenterReader.read(0x08, 1)[0] & 0xFF);
-//        telemetry.addData("5 ColorLeft", colorLeftReader.read(0x04, 1)[0] & 0xFF);
-//        telemetry.addData("6 ColorRight", colorRightReader.read(0x04, 1)[0] & 0xFF);
-        // telemetry.addData("7 Range", rangeReader.read(0x04, 2)[0] + " " + rangeReader.read(0x04, 2)[1]);
-        telemetry.addData("8 Motor", FL.getPower() + " " + FR.getPower() + " " + BL.getPower() + " " + BR.getPower());
-        telemetry.addData("9 Encoder", FL.getCurrentPosition() + " " + FR.getCurrentPosition() + " " + BL.getCurrentPosition() + " " + BR.getCurrentPosition());
-        telemetry.update();
-    }
-
-    private void setMode(DcMotor.RunMode r) {
-        FL.setMode(r);
-        FR.setMode(r);
-        BL.setMode(r);
-        BR.setMode(r);
-    }
-
-    private void setZeroMode(DcMotor.ZeroPowerBehavior z) {
-        FL.setZeroPowerBehavior(z);
-        FR.setZeroPowerBehavior(z);
-        BL.setZeroPowerBehavior(z);
-        BR.setZeroPowerBehavior(z);
-    }
-
-    private void setDrive(double p) {
-        FL.setPower(p);
-        FR.setPower(p);
-        BL.setPower(p);
-        BR.setPower(p);
-    }
-
-    private void setDrive(double p1, double p2) {
-        FL.setPower(p1);
-        FR.setPower(p2);
-        BL.setPower(p1);
-        BR.setPower(p2);
     }
 }
